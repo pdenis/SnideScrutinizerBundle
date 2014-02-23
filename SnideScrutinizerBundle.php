f054d50e655dc1b4b5d767c1b0e858d7e5403184
Pascal DENIS <pascal.denis@businessdecision.com>
2014-02-23 19:31:16 +0100

    init

---
+++ SnideScrutinizerBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Snide\Bundle\ScrutinizerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class SnideScrutinizerBundle
+ *
+ * @author Pascal DENIS <pascal.denis.75@gmail.com>
+ */
+class SnideScrutinizerBundle extends Bundle
+{
+}
