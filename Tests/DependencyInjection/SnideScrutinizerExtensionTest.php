4ec992778b630ec600b6a7dbd44fd80992338483
Pascal DENIS <pascal.denis@businessdecision.com>
2014-02-26 15:39:10 +0100

    dependencyInjection & twig extension test

---
+++ Tests/DependencyInjection/SnideScrutinizerExtensionTest.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * This file is part of the SnideTravinizer bundle.
+ *
+ * (c) Pascal DENIS <pascal.denis.75@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Snide\Bundle\ScrutinizerBundle\Tests\DependencyInjection;
+
+use Snide\Bundle\ScrutinizerBundle\DependencyInjection\SnideScrutinizerExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Yaml\Parser;
+
+/**
+ * Class SnideScrutinizerExtensionTest
+ *
+ * @author Pascal DENIS <pascal.denis.75@gmail.com>
+ */
+class SnideScrutinizerExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var ContainerBuilder */
+    protected $configuration;
+
+    /**
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function testLoadRepositorySlugThrowsExceptionUnless()
+    {
+        $loader = new SnideScrutinizerExtension();
+        $config = $this->getConfig();
+        unset($config['repository']['slug']);
+        $loader->load(array($config), new ContainerBuilder());
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function testLoadRepositoryTypeThrowsExceptionUnless()
+    {
+        $loader = new SnideScrutinizerExtension();
+        $config = $this->getConfig();
+
+        $config['repository']['type'] = 'svn';
+        $loader->load(array($config), new ContainerBuilder());
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function testLoadRepositoryThrowsExceptionUnlessSet()
+    {
+        $loader = new SnideScrutinizerExtension();
+        $config = $this->getConfig();
+        unset($config['repository']);
+        $loader->load(array($config), new ContainerBuilder());
+    }
+
+    /**
+     * Test load cache path
+     */
+    public function testLoadCachePath()
+    {
+        $this->loadConfiguration();
+        $this->assertEquals($this->configuration->getParameter('snide_scrutinizer.cache_path'), '/tmp');
+    }
+
+    /**
+     * Test load cache path
+     */
+    public function testLoadRepository()
+    {
+        $this->loadConfiguration();
+        $repository = $this->configuration->get('snide_scrutinizer.repository');
+
+        $this->assertEquals('pdenis/SnideScrutinizerBundle', $repository->getSlug());
+        $this->assertEquals('g', $repository->getType());
+    }
+
+    /**
+     * Load bundle configuration
+     */
+    protected function loadConfiguration()
+    {
+        $this->configuration = new ContainerBuilder();
+        $loader = new SnideScrutinizerExtension();
+        $config = $this->getConfig();
+        $loader->load(array($config), $this->configuration);
+    }
+    /**
+     * getConfig
+     *
+     * @return array
+     */
+    protected function getConfig()
+    {
+        $yaml = <<<EOF
+repository:
+    type: github
+    slug: pdenis/SnideScrutinizerBundle
+filesystem_cache_path: /tmp
+EOF;
+        $parser = new Parser();
+
+        return $parser->parse($yaml);
+    }
+}
---
+++ Tests/Twig/Extension/ScrutinizerExtensionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the SnideTravinizer bundle.
+ *
+ * (c) Pascal DENIS <pascal.denis.75@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Snide\Bundle\TravinizerBundle\Tests\Twig\Extension;
+
+use Snide\Bundle\ScrutinizerBundle\Twig\Extension\ScrutinizerExtension;
+
+/**
+ * Class GithubExtensionTest
+ *
+ * @author Pascal DENIS <pascal.denis.75@gmail.com>
+ */
+class GithubExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ScrutinizerExtension
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new ScrutinizerExtension();
+    }
+
+    /**
+     * Test getColor
+     */
+    public function testColor()
+    {
+        $this->assertEquals(ScrutinizerExtension::GREEN, $this->object->getColor(8));
+        $this->assertEquals(ScrutinizerExtension::GREEN, $this->object->getColor(9));
+        $this->assertEquals(ScrutinizerExtension::GREEN, $this->object->getColor(10));
+
+        $this->assertEquals(ScrutinizerExtension::YELLOW, $this->object->getColor(7));
+        $this->assertEquals(ScrutinizerExtension::YELLOW, $this->object->getColor(6));
+
+        $this->assertEquals(ScrutinizerExtension::SILVER, $this->object->getColor(5));
+        $this->assertEquals(ScrutinizerExtension::SILVER, $this->object->getColor(4));
+
+        $this->assertEquals(ScrutinizerExtension::RED, $this->object->getColor(3));
+        $this->assertEquals(ScrutinizerExtension::RED, $this->object->getColor(2));
+        $this->assertEquals(ScrutinizerExtension::RED, $this->object->getColor(1));
+        $this->assertEquals(ScrutinizerExtension::RED, $this->object->getColor(0));
+    }
+}
